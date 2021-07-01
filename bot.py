d72af2fd999526f83e03e9a68477dad7bf7f515e
Yonggan <30302975+Y0ngg4n@users.noreply.github.com>
2021-07-01 19:48:16 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -50,7 +50,7 @@ async def join_voice(ctx):
 		channel = ctx.author.voice.channel
 		await channel.connect()
 		await ctx.send("Sucessfully joined your voice channel")
-	except as e:
+	except Exception as e:
 		print(e)
 		await ctx.send(f"ERROR: You're not in a voice channel {ctx.message.author.mention}")
 
@@ -59,7 +59,7 @@ async def exit_voice(ctx):
 	try:
 		await ctx.voice_client.disconnect()
 		await ctx.send("Sucessfully left the voice channel")
-	except as e:
+	except Exception as e:
 		print(e)
 		await ctx.send(f"ERROR: {ctx.message.author.mention} You stupid? I'm not in a voice channel!")
 
@@ -72,7 +72,7 @@ async def play_payai(ctx):
 		if not vc.is_playing():
 			await ctx.send("PAAAAAAAAAAAYAAAAAAAAAAAI, ICH HAB GANIX GEMAKT")
 			vc.play(audio, after=None)
-	except as e:
+	except Exception as e:
 		print(e)
 		await ctx.send(f"Something went wrong playing the song...")
 
